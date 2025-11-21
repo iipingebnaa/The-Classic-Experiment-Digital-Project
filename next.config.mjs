75ff49632ce721d0553357dc698d565a09692275
Adelate <adelateiipinge@gmail.com>
2025-11-21 10:27:17 +0200

    Added PWA configuration to next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
+import { withPWA } from "next-pwa";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-   
-}
+  pwa: {
+    dest: "public",        // where service worker and PWA files will be generated
+    register: true,        // auto-register service worker
+    skipWaiting: true,     // activate SW immediately
+  },
+});
 
-export default nextConfig
+export default nextConfig;
