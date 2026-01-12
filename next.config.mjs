64d89cc912d4ae5859b7cb95ae70904cf144fcc3
Adelate <adelateiipinge@gmail.com>
2026-01-12 09:06:47 +0200

    Disabled static exports to allow dynamic rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",       // enables static export
+  //output: "export", // enables static export       
   typescript: {
     ignoreBuildErrors: true,
   },
