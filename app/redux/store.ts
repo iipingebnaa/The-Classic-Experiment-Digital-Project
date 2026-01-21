670608ed06a1bd68aec3f7ffa6d1c52b7630589c
Adelate <adelateiipinge@gmail.com>
2026-01-21 13:54:37 +0200

    Created redux store to store and manage the app's state data(17/01/2026)

---
+++ app/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
+import orderReducer from "./order/orderSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    order: orderReducer,
+   },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
