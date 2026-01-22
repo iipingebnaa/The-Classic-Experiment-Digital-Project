e3ec587a8c8465d3135397b6dbb0467bd651755e
Adelate <adelateiipinge@gmail.com>
2026-01-22 11:05:01 +0200

    Updated Redux store with uiReducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import orderReducer from "./order/orderSlice";
+import uiReducer from  "./ui/uiSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     order: orderReducer,
+    ui: uiReducer,
    },
 });
 
